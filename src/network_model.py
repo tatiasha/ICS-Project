224434d13e97a278a40590fee8eec671efea2bb2
Tatiana Kutuzova <25556503+tatiasha@users.noreply.github.com>
2018-01-17 01:14:02 +0300

    Update network_model.py

--- src/network_model.py
+++ src/network_model.py
@@ -3,90 +3,109 @@ import math
 import matplotlib.pyplot as plt
 import random
 
-probabilityP = 0.06
-probabilityQ = 0.05 #q<p
-probabilityR = 1.0-probabilityP-probabilityQ
-time = 10000
+def getModel(time, probabilityP, probabilityQ, per):
+    probabilityR = 1.0-probabilityP-probabilityQ
+    G = nx.DiGraph()
+    G.add_node(0)
+    per = time*(per/100.0)
+    """
+    At each time step
+    with probability p, a new node is created attaching to a directed node,
+    with probability q, a new node is created attached by a directed link 
+    with probability r, a directed link is created between the old nodes.
+    """
+    for step in range(time):
+        arrayNode = list(G.nodes)    
+        newIn = []
+        newOut = []
+        
+        for i in arrayNode:
+            if G.in_degree(i) > 0:
+                newIn.append(i)
+            if  G.out_degree(i) > per:
+                newOut.append(i)
 
-G = nx.DiGraph()
-G.add_node(0)
+        random_valueA = random.random()
+        random_valueB = random.random()
+        random_valueC = random.random()
 
-"""
-At each time step
-with probability p, a new node is created attaching to a directed node,
-with probability q, a new node is created attached by a directed link 
-with probability r, a directed link is created between the old nodes.
-"""
-for step in range(time):
-
-    arrayNode = list(G.nodes)
-    numOfNode = len(arrayNode)
-    arrayNodeDegree = []
-    allIO = 0 #all incoming/outgoing links in the network
-
-    newIn = []
-    newOut = []
-    for i in arrayNode:
-        allIO+=G.in_degree(i)
-        if G.in_degree(i) > 0:
-            newIn.append(i)
-        if  G.out_degree(i) > 3:
-            newOut.append(i)
-        arrayNodeDegree.append(G.degree(i))
-
-    random_valueA = random.random()
-    random_valueB = random.random()
-    random_valueC = random.random()
-
-    if random_valueA < probabilityP:
-        if newIn == []:
-            node_i = random.choice(arrayNode)
-        else:
-            node_i = random.choice(newIn)
-        G.add_node(step)
-        G.add_edge(node_i, step)
-
-    if random_valueB < probabilityQ:
-        if newOut == []:
-            node_i = random.choice(arrayNode)
-        else:
-            node_i = random.choice(newOut)
+        if random_valueA < probabilityP:
+            if newIn == []:
+                node_i = random.choice(arrayNode)
+            else:
+                node_i = random.choice(newIn)
             G.add_node(step)
-            G.add_edge(step, node_i)
+            G.add_edge(node_i, step)
 
-    if random_valueC < probabilityR:
-        if newOut+newIn == []:
-            node1 = random.choice(arrayNode)
-            node2 = random.choice(arrayNode)
-            G.add_edge(node1, node2)
-        else:
-            node1 = random.choice(newIn+newOut)
-            node2 = random.choice(newIn+newOut)
-            G.add_edge(node1, node2)
+        if random_valueB < probabilityQ:
+            if newOut == []:
+                node_i = random.choice(arrayNode)
+            else:
+                node_i = random.choice(newOut)
+                G.add_node(step)
+                G.add_edge(step, node_i)
 
+        if random_valueC < probabilityR:
+            if newOut+newIn == []:
+                node1 = random.choice(arrayNode)
+                node2 = random.choice(arrayNode)
+                G.add_edge(node1, node2)
+            else:
+                node1 = random.choice(newIn+newOut)
+                node2 = random.choice(newIn+newOut)
+                G.add_edge(node1, node2)
 
-#nx.draw(G, with_labels=True, node_color = "blue", node_shape='o', alpha = 0.9, linewidths = 6)
-#plt.show()
+    #nx.draw(G, with_labels=True, node_color = "blue", node_shape='o', alpha = 0.9, linewidths = 6)
+    #plt.show()
+    return G
 
-ba_c = [G.degree(x) for x in G.nodes]
-ba_c.sort()
-y = list(set(ba_c))
-y.sort()
+def getLambda(degrees):
+    exp = 0
+    for d in degrees:
+        exp+=math.log10(d/degrees[0])
+    exp = 1+ len(degrees)/exp
+    return exp
 
-exp = 0
-for d in y:
-    exp+=math.log10(d/y[0])
-exp = 1+ len(y)/exp
-print ("Lambda", exp)
-
-plt.xscale('log')
-plt.yscale('log')
-x = y
-yA = [ba_c.count(y[i])/float(len(ba_c)) for i in range(len(y))]
-plt.scatter(x,yA,c='b',marker='x', label = "Users activity")
-ylambd = [i**-exp for i in y]
-plt.plot(y, ylambd, label='Lambda = -'+str(exp), color = "orange")
-plt.xlabel('Probability (log)')
-plt.ylabel('Users count (log)')
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-plt.show()
+def plotDegree(exp, networkDegrees, networkDegreesList):
+    lengthDL = len(networkDegreesList)
+    lengthD = len(networkDegrees)
+    
+    probability = [networkDegrees.count(networkDegreesList[i])/float(lengthD) for i in range(lengthDL)]
+    ylambd = [j**-exp for j in networkDegreesList]
+    
+    plt.scatter(networkDegreesList,probability,c='b',marker='x', label = "Users activity")
+    plt.plot(networkDegreesList, ylambd, label='Lambda = -'+str(exp), color = "orange")
+    
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('Degree (log)')
+    plt.ylabel('Probability (log)')
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.show()
+    
+def saveNetwork(path, G):
+    import csv
+    f = open(path,'w')
+    f.write('\n') # We write the blank line at the top
+    writerN = csv.writer(f,delimiter=',')
+    writerN.writerow(['Source','Target'])
+    writerN.writerows(G.edges())
+    f.close()
+    
+ if __name__=="__main__":
+    probabilityP = 0.067
+    probabilityQ = 0.042 #q<p
+    time = 10000
+    per = 0.1
+    network = getModel(time, probabilityP, probabilityQ, per)
+    
+    networkDegrees = [network.degree(x) for x in network.nodes]
+    networkDegrees.sort()
+    networkDegreesList = list(set(networkDegrees))
+    networkDegreesList.sort()
+    
+    lmd = getLambda(networkDegreesList)
+    plotDegree(lmd, networkDegrees, networkDegreesList)
+    
+    #path = "/Users/tatiana/Documents/Magistracy/1 semestr/Introduction to Computer Science/Project/nts.csv"
+    #saveNetwork(path, network)
